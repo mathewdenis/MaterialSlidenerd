ef3a4b20ebe542242fb32e462ed396e93244e7ce
Denis Mathew <denis.mathew@gmail.com>
2016-02-06 17:28:55 +0530

    Update MainActivity.java

--- app/src/main/java/materialslidenerd/com/materialslidenerd/MainActivity.java
+++ app/src/main/java/materialslidenerd/com/materialslidenerd/MainActivity.java
@@ -2,12 +2,12 @@ package materialslidenerd.com.materialslidenerd;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
     @Override
